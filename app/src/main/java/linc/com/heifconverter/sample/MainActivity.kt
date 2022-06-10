946a0f1feed929865f3e9f01e7ed68c716e186d2
Jordon de Hoog <jordon.dehoog@gmail.com>
2022-06-10 16:00:21 -0400

    remove deprecated 'kotlin-android-extensions'

--- app/src/main/java/linc/com/heifconverter/sample/MainActivity.kt
+++ app/src/main/java/linc/com/heifconverter/sample/MainActivity.kt
@@ -2,8 +2,9 @@ package linc.com.heifconverter.sample
 
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 import linc.com.heifconverter.HeifConverter
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val convert = findViewById<Button>(R.id.convert)
+        val resultImage = findViewById<ImageView>(R.id.resultImage)
 
         convert.setOnClickListener {
             HeifConverter.useContext(this)
